43952a6446d6daf0dc3a059505209f9be6dd9400
Rohit Kumar <ngp1005@NGPs-MacBook-Pro-2.local>
2023-11-30 15:44:45 +0530

    added table

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,143 +1,161 @@
-// import React, { useState } from "react";
-// import Box from "@mui/material/Box";
-// import { TextField, Typography, Button } from "@mui/material";
-// import Table from "@mui/material/Table";
-// import TableBody from "@mui/material/TableBody";
-// import TableCell from "@mui/material/TableCell";
-// import TableContainer from "@mui/material/TableContainer";
-// import TableHead from "@mui/material/TableHead";
-// import TableRow from "@mui/material/TableRow";
-// import Paper from "@mui/material/Paper";
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Box,
+} from "@mui/material";
 
-// let data = [
-//   {
-//     name: "Rohit",
-//   },
-//   {
-//     name: "Mohan",
-//   },
-//   {
-//     name: "Ram",
-//   },
-//   {
-//     name: "Rohit",
-//   },
-//   {
-//     name: "India",
-//   },
-//   {
-//     name: "India",
-//   },
-//   {
-//     name: "sanit",
-//   },
-//   {
-//     name: "sanit",
-//   },
-//   {
-//     name: "sanit",
-//   },
-// ];
+interface DataEntry {
+  name: string;
+}
 
-// const Form = () => {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <Box
-//       sx={{
-//         width: { lg: "40%", md: "60%", sm: "80%", xs: "90%" },
-//         textAlign: "center",
-//         margin: "20px auto",
-//       }}
-//     >
-//       <Box sx={{ margin: "30px 0px" }}>
-//         <TableContainer component={Paper}>
-//           <Table sx={{ minWidth: 350 }} aria-label="simple table">
-//             {data.length > 0 ? (
-//               <TableHead>
-//                 <TableRow>
-//                   <TableCell>Name</TableCell>
-//                   <TableCell align="right">number of time</TableCell>
-//                 </TableRow>
-//               </TableHead>
-//             ) : null}
-//             <TableBody>
-//               {data?.map((item: any, index: number) => {
-//                 return (
-//                   <>
-//                     <TableRow
-//                       key={index}
-//                       sx={{
-//                         "&:last-child td, &:last-child th": { border: 0 },
-//                       }}
-//                     >
-//                       <TableCell component="th" scope="row">
-//                         {item.name}
-//                       </TableCell>
-//                       <TableCell align="right">{item.email}</TableCell>
-//                     </TableRow>
-//                   </>
-//                 );
-//               })}
-//             </TableBody>
-//           </Table>
-//         </TableContainer>
-//       </Box>
-//     </Box>
-//   );
-// };
-
-// export default Form;
-
-import React, { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import { Button, Typography } from "@mui/material";
+interface DistinctNameEntry {
+  name: string;
+  count: number;
+}
 
 const Form: React.FC = () => {
-  const [sec, setSec] = useState<number>(300);
-  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [data, setData] = useState<DataEntry[]>([
+    { name: "Rohit" },
+    { name: "Bhuvi" },
+    { name: "Rohit" },
+    { name: "Bhuvi" },
+    { name: "Mohan" },
+    { name: "Mohan" },
+    { name: "Mohan" },
+    { name: "Mohan" },
+    { name: "Mohan" },
+    { name: "Mohan" },
+    { name: "Ram" },
+    { name: "Ram" },
+    { name: "Ram" },
+    { name: "Ram" },
+    { name: "Ram" },
+    { name: "Ram" },
+    { name: "Ram" },
+    { name: "Ram" },
+    { name: "Ram" },
+    { name: "Ram" },
+    { name: "Ram" },
+  ]);
 
-  useEffect(() => {
-    let interval: any;
-    if (isRunning && sec > 0) {
-      interval = setInterval(() => {
-        setSec((prevSec) => prevSec - 1);
-      }, 1000);
+  const getDistinctNamesWithCounts = (): DistinctNameEntry[] => {
+    const nameCounts: Record<string, number> = data.reduce(
+      (acc: any, entry) => {
+        const name = entry.name;
+        acc[name] = (acc[name] || 0) + 1;
+        return acc;
+      },
+      {}
+    );
+
+    const distinctNames: DistinctNameEntry[] = Object.keys(nameCounts).map(
+      (name) => ({
+        name,
+        count: nameCounts[name],
+      })
+    );
+
+    return distinctNames;
+  };
+
+  const distinctNames: DistinctNameEntry[] = getDistinctNamesWithCounts();
+
+  const getBackgroundColor = (count: number): string => {
+    if (0 < count && count < 3) {
+      return "red";
+    } else if (2 < count && count < 10) {
+      return "yellow";
+    } else if (count >= 10) {
+      return "green";
     } else {
-      clearInterval(interval);
-      setIsRunning(false);
+      return "";
     }
-
-    return () => clearInterval(interval);
-  }, [sec, isRunning]);
-
-  const handleStart = () => {
-    setIsRunning(true);
-  };
-
-  const handleReset = () => {
-    setIsRunning(false);
-    setSec(300);
-  };
-
-  const formatTime = (timeInSeconds: number): string => {
-    const minutes: number = Math.floor(timeInSeconds / 60);
-    const seconds: number = timeInSeconds % 60;
-    return `${minutes} min ${seconds} sec`;
   };
 
   return (
-    <Box sx={{ width: "40%", margin: "30px auto" }}>
-      <Typography>StopWatch {formatTime(sec)}</Typography>
-
-      <Button onClick={handleStart} sx={{ margin: "10px" }}>
-        Start
-      </Button>
-      <Button onClick={handleReset} sx={{ margin: "10px" }}>
-        Reset
-      </Button>
+    <Box style={{ width: "40%", margin: "10px auto" }}>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Count</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {distinctNames.map((entry, index) => (
+              <TableRow
+                key={index}
+                style={{ backgroundColor: getBackgroundColor(entry.count) }}
+              >
+                <TableCell>{entry.name}</TableCell>
+                <TableCell>{entry.count}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
 
 export default Form;
+
+// import React, { useEffect, useState } from "react";
+// import Box from "@mui/material/Box";
+// import { Button, Typography } from "@mui/material";
+
+// const Form: React.FC = () => {
+//   const [sec, setSec] = useState<number>(300);
+//   const [isRunning, setIsRunning] = useState<boolean>(false);
+
+//   useEffect(() => {
+//     let interval: any;
+//     if (isRunning && sec > 0) {
+//       interval = setInterval(() => {
+//         setSec((prevSec) => prevSec - 1);
+//       }, 1000);
+//     } else {
+//       clearInterval(interval);
+//       setIsRunning(false);
+//     }
+
+//     return () => clearInterval(interval);
+//   }, [sec, isRunning]);
+
+//   const handleStart = () => {
+//     setIsRunning(true);
+//   };
+
+//   const handleReset = () => {
+//     setIsRunning(false);
+//     setSec(300);
+//   };
+
+//   const formatTime = (timeInSeconds: number): string => {
+//     const minutes: number = Math.floor(timeInSeconds / 60);
+//     const seconds: number = timeInSeconds % 60;
+//     return `${minutes} min ${seconds} sec`;
+//   };
+
+//   return (
+//     <Box sx={{ width: "40%", margin: "30px auto" }}>
+//       <Typography>StopWatch {formatTime(sec)}</Typography>
+
+//       <Button onClick={handleStart} sx={{ margin: "10px" }}>
+//         Start
+//       </Button>
+//       <Button onClick={handleReset} sx={{ margin: "10px" }}>
+//         Reset
+//       </Button>
+//     </Box>
+//   );
+// };
+
+// export default Form;
